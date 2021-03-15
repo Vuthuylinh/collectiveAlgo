cbaf9c9fc8add470afd7b123251da81ee11d55f8
Linh Vu <thuylinh2490@gmail.com>
2021-03-14 23:40:16 -0400

    array(mergeing2Packages) prompt

---
+++ array/merging2Packages.js
@@ -0,0 +1,8 @@
+/*
+ Given a package with a weight limit - limit
+ and an array (arr) of item weights, implement a function getIndicesOfItemWeights that finds two items who sum of weights equals the weight limit (limit).
+ Your fucntion should return a pair [i,j] of the indicies of the items weights, orsers such that i>j.
+ If such a pair doesn't exist, reutrn an empty array.
+
+ Analize the time and space complexity 
+*/
